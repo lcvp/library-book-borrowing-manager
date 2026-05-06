52a6b4f04c26114e79e9545f15513551df02629c
lcvp <221131646+lcvp@users.noreply.github.com>
2026-05-06 13:18:37 +0700

    chore(address.h): add file header

--- include/library_book_borrowing_manager/domain/address.h
+++ include/library_book_borrowing_manager/domain/address.h
@@ -1,7 +1,25 @@
+// This program is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or (at your option) any later
+// version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+// details.
+//
+// You should have received a copy of the GNU General Public License along with
+// this program. If not, see <https://www.gnu.org/licenses/>.
+//
+// Defines the Address class for Library Book Borrowing Manager. Intended to be
+// used by the Customer class.
+
 #ifndef LIBRARY_BOOK_BORROWING_MANAGER_DOMAIN_ADDRESS_H_
 #define LIBRARY_BOOK_BORROWING_MANAGER_DOMAIN_ADDRESS_H_
 
 #include <string>
+
+namespace library_book_borrowing_manager::domain {
 
 class Address {
 public:
@@ -23,5 +41,7 @@ private:
   std::string city_;
   std::string postal_code_;
 };
+
+} // namespace library_book_borrowing_manager::domain
 
 #endif // LIBRARY_BOOK_BORROWING_MANAGER_DOMAIN_ADDRESS_H_
