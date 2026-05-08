b397cbfb72ae4e53d309128c5e5b8cfb982784e4
lcvp <221131646+lcvp@users.noreply.github.com>
2026-05-08 19:19:26 +0700

    feat(domain): initialize CitizenID

---
+++ include/library_book_borrowing_manager/domain/citizen_id.h
@@ -0,0 +1,39 @@
+// This program is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or (at your option) any later
+// version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+// details.
+//
+// You should have received a copy of the GNU General Public License along with
+// this program. If not, see <https://www.gnu.org/licenses/>.
+//
+// Defines the CitizenId class. Intended to be used by the Customer class.
+
+#ifndef LIBRARY_BOOK_BORROWING_MANAGER_DOMAIN_CITIZEN_ID_H_
+#define LIBRARY_BOOK_BORROWING_MANAGER_DOMAIN_CITIZEN_ID_H_
+
+#include <string>
+
+namespace library_book_borrowing_manager::domain {
+
+class CitizenId {
+ public:
+  explicit CitizenId(std::string id);
+
+  std::string id() const;
+
+  void set_id(std::string id);
+
+  static bool IsValid(std::string id);
+
+ private:
+  std::string id_;
+};
+
+}  // namespace library_book_borrowing_manager::domain
+
+#endif  // LIBRARY_BOOK_BORROWING_MANAGER_DOMAIN_CITIZEN_ID_H_
