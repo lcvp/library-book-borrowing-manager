74e6165c4650947e9cf09a5e3329856af575fbd6
PHẠM QUANG KHẢI <phamqkhai1204@gmail.com>
2026-05-10 11:31:48 +0700

    feat(domain): declare thesis class header (#4)

---
+++ include/library_book_borrowing_manager/domain/thesis.h
@@ -0,0 +1,53 @@
+// This program is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or (at your option) any later
+// version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+// details.
+//
+// You should have received a copy of the GNU General Public License along with
+// this program. If not, see <https://www.gnu.org/licenses/>.
+//
+// Defines the thesis class. Inherits from Title.
+
+#ifndef LIBRARY_BOOK_BORROWING_MANAGER_DOMAIN_THESIS_H_
+#define LIBRARY_BOOK_BORROWING_MANAGER_DOMAIN_THESIS_H_
+
+#include <string>
+
+#include "library_book_borrowing_manager/domain/title.h"
+
+namespace library_book_borrowing_manager::domain {
+
+class Thesis : public Title {
+ public:
+  Thesis(std::string id, std::string name, std::string description,
+         std::string author, std::string publisher, std::string university,
+         std::string degree_level, int defense_year,
+         std::string supervisor_name);
+
+  void set_university(std::string university);
+  void set_degree_level(std::string degree_level);
+  void set_defense_year(int defense_year);
+  void set_supervisor_name(std::string supervisor_name);
+
+  std::string university() const;
+  std::string degree_level() const;
+  int defense_year() const;
+  std::string supervisor_name() const;
+
+  std::string GetApaCitation() const override;
+
+ private:
+  std::string university_;
+  std::string degree_level_;
+  int defense_year_;
+  std::string supervisor_name_;
+};
+
+}  // namespace library_book_borrowing_manager::domain
+
+#endif  // LIBRARY_BOOK_BORROWING_MANAGER_DOMAIN_THESIS_H_F
