9d376320a4d26a9f240575db8299522801dbcdb7
lcvp <221131646+lcvp@users.noreply.github.com>
2026-05-12 11:28:48 +0700

    feat(domain): implement ItemRepository class

--- src/domain/repositories/item_repository.cpp
+++ src/domain/repositories/item_repository.cpp
@@ -10,3 +10,11 @@
 //
 // You should have received a copy of the GNU General Public License along with
 // this program. If not, see <https://www.gnu.org/licenses/>.
+
+#include "library_book_borrowing_manager/domain/repositories/item_repository.h"
+
+namespace library_book_borrowing_manager::domain::repositories {
+
+ItemRepository::~ItemRepository() = default;
+
+}  // namespace library_book_borrowing_manager::domain::repositories
