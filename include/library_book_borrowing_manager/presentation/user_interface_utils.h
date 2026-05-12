ccfecb9ff250ae4f941770ad43b0d725618a43ee
lcvp <221131646+lcvp@users.noreply.github.com>
2026-05-12 08:48:57 +0700

    chore: add license notices (#15)

--- include/library_book_borrowing_manager/presentation/user_interface_utils.h
+++ include/library_book_borrowing_manager/presentation/user_interface_utils.h
@@ -10,3 +10,9 @@
 //
 // You should have received a copy of the GNU General Public License along with
 // this program. If not, see <https://www.gnu.org/licenses/>.
+//
+// -----------------------------------------------------------------------------
+// user_interface_utils.h
+// -----------------------------------------------------------------------------
+//
+//
