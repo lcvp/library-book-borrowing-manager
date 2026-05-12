65d099884994dd628b57de41c4dfae76e4e1968d
lcvp <221131646+lcvp@users.noreply.github.com>
2026-05-12 11:28:30 +0700

    feat(domain): implement CustomerRepository class

--- src/domain/repositories/customer_repository.cpp
+++ src/domain/repositories/customer_repository.cpp
@@ -10,3 +10,11 @@
 //
 // You should have received a copy of the GNU General Public License along with
 // this program. If not, see <https://www.gnu.org/licenses/>.
+
+#include "library_book_borrowing_manager/domain/repositories/customer_repository.h"
+
+namespace library_book_borrowing_manager::domain::repositories {
+
+CustomerRepository::~CustomerRepository() = default;
+
+}  // namespace library_book_borrowing_manager::domain::repositories
