82a9e1b7bc0127ce92d9d9a23779ef4cbe6a663c
hhoang-219 <nguyenhaihoang210906@gmail.com>
2026-05-13 15:46:20 +0700

    feat(domain): implement Email class definition (#26)

--- src/domain/email.cpp
+++ src/domain/email.cpp
@@ -10,3 +10,40 @@
 //
 // You should have received a copy of the GNU General Public License along with
 // this program. If not, see <https://www.gnu.org/licenses/>.
+
+
+#include "library_book_borrowing_manager/domain/email.h"
+
+#include <regex>
+#include <stdexcept>
+#include <string>
+#include <utility>
+
+namespace library_book_borrowing_manager::domain {
+
+Email::Email(std::string email_address) {
+  if (!IsValid(email_address)) {
+    throw std::invalid_argument("Invalid email address format.");
+  }
+  email_address_ = email_address;
+}
+
+std::string Email::email_address() const { return email_address_; }
+
+void Email::set_email_address(std::string email_address) {
+  if (!IsValid(email_address)) {
+    throw std::invalid_argument("Invalid email address format.");
+  }
+  email_address_ = email_address;
+}
+
+bool Email::IsValid(std::string email_address) {
+  const std::regex pattern(
+      R"(^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$)");
+  return std::regex_match(email_address, pattern);
+}
+
+}  // namespace library_book_borrowing_manager::domain
+
+
+  
